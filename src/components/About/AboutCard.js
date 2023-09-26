4f648d39010ef1e68ff30759af8a40257f0ea678
Wahit Fitriyanto <26300406+wahitftry@users.noreply.github.com>
2023-09-26 22:28:45 +0700

    test

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,13 +8,14 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br /> I am a final year student pursuing an Integrated MSc (IMSc)
-            in Maths and Computing at BIT Mesra.
+            Hi Everyone, I am <span className="purple">Wahit Fitriyanto </span>
+            from <span className="purple"> Jawa Tengah, Indonesia.</span>
+            <br /> I am Wahit Fitriyanto, an informatics engineering student at Boyolali University. I have great interest in web development, data structures, and algorithms. I am an enthusiastic learner and always want to learn and work in various technologies and domains. I love exploring new technologies and leveraging them to solve real problems. Apart from that, I also like learning new things that I have never learned before.
             <br />
-            Additionally, I am currently employed as a software developer at
-            Juspay.
+            I actively contribute on Github, where I share my projects related to Nodejs, Python, React, TypeScript, PHP, MySQL, and others. I also have a personal blog where I write about technical topics that interest me. I also often participate in hackathons, meetups, and other technology events.
+            <br />
+            <br />
+            I am a friendly and sociable person. I enjoy sharing knowledge and experiences with others. I also have a good sense of humor and like to joke. I am an optimist and always try to improve myself.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
@@ -24,17 +25,17 @@ function AboutCard() {
               <ImPointRight /> Playing Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Listening to songs
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> Talking and hangout with friends
             </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Wahit Fitriyanto</footer>
         </blockquote>
       </Card.Body>
     </Card>
